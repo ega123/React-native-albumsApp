e7b54632459682d255029fc65b8acaefc7c67360
Shiirevjamts Munkhbat <shiirevjamtsmunkhbat@Shiirevjamtss-MacBook-Pro.local>
2018-06-14 19:04:30 -0500

    Second commit

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 //Import a library to help create components
 import React from 'react';
-import { AppRegistry } from 'react-native';
-import Header from './src/components/header';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
+
 // create a component
 
 const App = () => {
-  return <Header />;
+  return (
+    <View>
+      <Header headerText={'Albums'} />
+      <AlbumList />
+    </View>
+  );
 };
 
 // Render it to a device
